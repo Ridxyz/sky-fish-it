f4a90a83a77dc19d702c46b0812d9d18a5e5a4be
Ridxyz <faridgunawan2008@gmail.com>
2026-01-17 17:06:55 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,2 +1,12 @@
-print("Loading MyScriptBase...")
-loadstring(readfile("MyScriptBase/Main.lua"))()
+local games = {
+    [121864768012064] = "https://raw.githubusercontent.com/Ridxyz/MyScriptBase/main/Main.lua"
+}
+
+local currentID = game.PlaceId
+local scriptURL = games[currentID]
+
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
+else
+    print("Game tidak terdaftar di loader.")
+end
