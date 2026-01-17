8d09fb98d599390060fa8a8926476b5fe5389dfe
Ridxyz <faridgunawan2008@gmail.com>
2026-01-17 16:49:48 +0700

    Add initial configuration settings in Config.lua

---
+++ Config.lua
@@ -0,0 +1,18 @@
+Config = {
+    Teleport = {
+        Enabled = false,
+        Target = "Spawn"
+    },
+
+    Farming = {
+        Enabled = false,
+        Delay = 1.0
+    },
+
+    Fishing = {
+        Enabled = false,
+        Speed = 0.5
+    }
+}
+
+return Config
